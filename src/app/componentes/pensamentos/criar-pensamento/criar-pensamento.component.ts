3df017af1599aa5db583ade946a45c124728a9ac
mauricioKaneko <mauricio.kaneko@gmail.com>
2023-08-07 14:21:35 -0300

    Criando routers e lista de pensamentos

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent implements OnInit {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'dev',
+    modelo:'modelo1'
+  }
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  criarPensamento(){
+    alert("Novo Pensamento Criado");
+  }
+
+  cancelar(){
+    alert("botao cancelar");
   }
 
 }
